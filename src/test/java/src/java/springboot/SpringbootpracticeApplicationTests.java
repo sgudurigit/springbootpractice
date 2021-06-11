21f6016fecc7d5f9194c993b06d7d52cbd129fb3
sravan <sravan1112@gmail.com>
2021-06-11 11:33:24 -0400

    Adding first project into git

---
+++ src/test/java/src/java/springboot/SpringbootpracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package src.java.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootpracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
